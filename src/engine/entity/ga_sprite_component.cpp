a41c7ae7905589d9e10deb2d44741ef88e26f79b
Ben Chang <changb3@rpi.edu>
2019-01-27 20:27:09 -0500

    add ga_sprite_component base

---
+++ src/engine/entity/ga_sprite_component.cpp
@@ -0,0 +1,16 @@
+#include "ga_sprite_component.h"
+
+ga_sprite_component::ga_sprite_component(ga_entity* ent) : ga_component(ent)
+{
+
+}
+
+ga_sprite_component::~ga_sprite_component()
+{
+
+}
+
+void ga_sprite_component::update(ga_frame_params* params)
+{
+
+}
---
+++ src/engine/entity/ga_sprite_component.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ga_component.h"
+
+class ga_sprite_component : public ga_component
+{
+public:
+	ga_sprite_component(class ga_entity* ent);
+	~ga_sprite_component();
+
+	virtual void update(struct ga_frame_params* params) override;
+
+};
