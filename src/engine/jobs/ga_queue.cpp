967aa5fcbda3b1b8a196f1475091db91a0e9b0dc
Ben Chang <changb3@rpi.edu>
2019-01-27 19:42:11 -0500

    my queue implementation

--- src/engine/jobs/ga_queue.cpp
+++ src/engine/jobs/ga_queue.cpp
@@ -9,20 +9,139 @@
 
 #include "ga_queue.h"
 
+
+bool operator==(const ga_queue_node_pointer_t a, const ga_queue_node_pointer_t b)
+{
+	return ((a._ptr == b._ptr) && (a._count == b._count));
+}
+
+// implementing the lock-free version
+
+ga_queue::ga_queue(int node_count)
+{
+	ga_queue_node_t* node = new ga_queue_node_t(NULL,NULL);
+	ga_queue_node_pointer_t p;
+	p._ptr = node;
+	p._count = 0;
+
+	_head = p;
+	_tail = p;
+	_count = 0;
+}
+
+ga_queue::~ga_queue()
+{
+
+}
+
+bool ga_queue::CAS(std::atomic<ga_queue_node_pointer_t>* variable, ga_queue_node_pointer_t* current_value, ga_queue_node_t* node, uint32_t count)
+{
+	ga_queue_node_pointer_t new_value;
+	new_value._ptr = node;
+	new_value._count = count;
+	return std::atomic_compare_exchange_weak(variable, current_value, new_value);
+}
+
+void ga_queue::push(void* data)
+{
+	ga_queue_node_t* node = new ga_queue_node_t (data, NULL);
+	ga_queue_node_pointer_t tail;
+	for (;;)
+	{
+		tail = _tail.load();
+		ga_queue_node_pointer_t next = tail._ptr->_next;
+		
+		if (tail==_tail.load())	// are tail and next consistent?
+		{
+			if (next._ptr == NULL)	// was tail pointing to the last node?
+			{
+				if (CAS(&tail._ptr->_next, &next, node, next._count+1))
+				{
+					break;
+				}
+			}
+			else
+			{
+				CAS(&_tail, &tail, next._ptr, tail._count + 1);
+			}
+		}
+	}
+	CAS(&_tail, &tail, node, tail._count + 1);
+	_count++;
+}
+
+
+bool ga_queue::pop(void** data)
+{
+	ga_queue_node_pointer_t head = _head.load();
+	ga_queue_node_pointer_t tail = _tail.load();
+	for (;;)
+	{
+		head = _head.load();
+		tail = _tail.load();
+		ga_queue_node_pointer_t next = head._ptr->_next;
+		if (head == _head.load())
+		{
+			if (head._ptr == tail._ptr)		// is queue empty or tail falling behind?
+			{
+				if (next._ptr == NULL)		// is queue empty?
+				{
+					return false;			// queue is empty, couldn't pop
+				}
+				CAS(&_tail, &tail, next._ptr, tail._count + 1);
+			}
+			else
+			{
+				//read value before CAS, otherwise another dequeue might free the next node
+				*data = next._ptr->_data;
+				if (CAS(&_head, &head, next._ptr, head._count + 1))
+				{
+					break;
+				}
+			}
+		}
+	}
+	free(head._ptr);
+	return true;
+}
+
+int ga_queue::get_count() const
+{
+
+	return _count;
+}
+
+
+
+/*
+// implementing the dual-lock version
+// how to use lock guard: std::lock_guard<std::mutex> lock(g_i_mutex);
+
+
 ga_queue::ga_queue(int node_count)
 {
 	// TODO:
 	// Initialize the queue.
-	// For extra credit, preallocate 'node_count' elements (instead of
-	// allocating on push).
-	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+
+	ga_queue_node_t *node = new ga_queue_node_t();
+	node->_next = NULL;
+	_head = node;
+	_tail = node;
+	_count = 0;
+
 }
 
 ga_queue::~ga_queue()
 {
 	// TODO:
 	// Free any resources held by the queue.
-	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+
+	void* data;
+	while (pop(&data))
+	{
+		;
+	}
+	
 }
 
 void ga_queue::push(void* data)
@@ -32,7 +151,17 @@ void ga_queue::push(void* data)
 	// If you preallocated 'node_count' elements, and if the queue is full when
 	// this function is called, you must block until another thread pops an
 	// element off the queue.
-	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+	
+	ga_queue_node_t *node = new ga_queue_node_t();
+	node->data = data;
+	node->_next = NULL;
+
+	{
+		std::lock_guard<std::mutex> lock(_tail_mutex);
+		_tail->_next = node;
+		_tail = node;
+		_count++;
+	}
 }
 
 bool ga_queue::pop(void** data)
@@ -42,13 +171,30 @@ bool ga_queue::pop(void** data)
 	// the memory pointed to by 'data'.
 	// If the queue is empty when this function is called, return false.
 	// Otherwise return true.
-	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
-	return false;
+
+	{
+		std::lock_guard<std::mutex> lock(_head_mutex);
+		ga_queue_node_t *node = _head;
+		ga_queue_node_t *new_head = _head->_next;
+		if (new_head == NULL)	// is queue empty?
+		{
+			return false;
+		}
+		else
+		{
+			*data = new_head->data;
+			_head = new_head;
+			free(node);
+			_count--;
+		}
+	}
+	return true;
 }
 
 int ga_queue::get_count() const
 {
 	// TODO:
 	// Get the number of elements currently in the queue.
-	return 0;
+	return _count;
 }
+*/
--- src/engine/jobs/ga_queue.h
+++ src/engine/jobs/ga_queue.h
@@ -11,8 +11,34 @@
 
 /*
 ** Thread-safe queue.
-** https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+** implementing the locking version
 */
+
+#include <mutex>
+#include <atomic>
+
+struct ga_queue_node_pointer_t
+{
+	struct ga_queue_node_t* _ptr;
+	uint32_t _count;
+};
+
+struct ga_queue_node_t
+{
+	void *_data;
+	std::atomic<ga_queue_node_pointer_t> _next;
+	ga_queue_node_t()
+	{
+		_data = NULL;
+	}
+	ga_queue_node_t(void *data, ga_queue_node_t* nextPtr) : _data(data)
+	{
+		ga_queue_node_pointer_t n;
+		n._ptr = nextPtr;
+		_next = n;
+	}
+};
+
 class ga_queue
 {
 public:
@@ -23,4 +49,13 @@ public:
 	bool pop(void** data);
 
 	int get_count() const;
+
+private:
+	std::atomic<ga_queue_node_pointer_t> _head;
+	std::atomic<ga_queue_node_pointer_t> _tail;
+	std::atomic<int> _count;
+	bool CAS(std::atomic<ga_queue_node_pointer_t>* variable, ga_queue_node_pointer_t* current_value, ga_queue_node_t* node, uint32_t count);
 };
+
+
+
