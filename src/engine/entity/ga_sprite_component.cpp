367a24a219615f654017a5603304d7b756a22504
Ben Chang <changb3@rpi.edu>
2019-01-27 20:34:25 -0500

    added set_position and set_rect methods to sprite_component

--- src/engine/entity/ga_sprite_component.cpp
+++ src/engine/entity/ga_sprite_component.cpp
@@ -1,7 +1,13 @@
 #include "ga_sprite_component.h"
 
-ga_sprite_component::ga_sprite_component(ga_entity* ent) : ga_component(ent)
+ga_sprite_component::ga_sprite_component(ga_entity* ent, int x, int y) : ga_component(ent)
 {
+	_x = x;
+	_y = y;
+	_rect.x = x;
+	_rect.y = y;
+	_rect.w = 0;
+	_rect.h = 0;
 
 }
 
@@ -13,4 +19,23 @@ ga_sprite_component::~ga_sprite_component()
 void ga_sprite_component::update(ga_frame_params* params)
 {
 
+}
+
+void ga_sprite_component::set_position(int x, int y)
+{
+	_x = x;
+	_y = y;
+	_rect.x = x;
+	_rect.y = y;
+
+}
+
+void ga_sprite_component::set_rect(int x, int y, int w, int h)
+{
+	_x = x;
+	_y = y;
+	_rect.x = x;
+	_rect.y = y;
+	_rect.w = w;
+	_rect.h = h;
 }
--- src/engine/entity/ga_sprite_component.h
+++ src/engine/entity/ga_sprite_component.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "ga_component.h"
-
+#include <SDL.h>
 class ga_sprite_component : public ga_component
 {
 public:
-	ga_sprite_component(class ga_entity* ent);
+	int _x;			// position on screen
+	int _y;
+	SDL_Rect _rect; // bounds rect on screen
+
+	ga_sprite_component(class ga_entity* ent, int x, int y);
 	~ga_sprite_component();
 
 	virtual void update(struct ga_frame_params* params) override;
+	void set_position(int x, int y);
+	void set_rect(int x, int y, int w, int h);
+
 
 };
